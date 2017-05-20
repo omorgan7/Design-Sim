8f15de967356ef1a49f47d1c70ba83a60532cb96
Owen Morgan <o.morgan@bath.ac.uk>
2017-05-20 12:54:03 +0100

    Initialised the project.

---
+++ Assets/Scripts/Decline.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Decline : MonoBehaviour {
+	public GameObject parent;
+	public Button yourButton;
+
+    void Start(){
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+	// Update is called once per frame
+	void TaskOnClick(){
+		Destroy(parent);
+	}
+}
---
+++ Assets/Scripts/SpawnBrief.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBrief : MonoBehaviour {
+
+	public float spawnChanceInitial = 0.01f;
+	private float briefSpawnChance;
+	public float chanceIncrease = 0.001f;
+	public GameObject ModalBox;
+	private GameObject box;
+
+	// Update is called once per frame
+	void Start(){
+		briefSpawnChance = spawnChanceInitial;
+	}
+	void Update () {
+		if(box == null){
+			briefSpawnChance += chanceIncrease;
+			float randomNum = Random.value;
+			if(randomNum <= briefSpawnChance){
+				box = Instantiate(ModalBox,Vector3.zero,Quaternion.identity);
+				briefSpawnChance = spawnChanceInitial;
+			}
+		}
+	}
+}
