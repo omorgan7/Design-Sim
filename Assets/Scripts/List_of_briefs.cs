5e4044b8c7cd4012c96ee06f240381bb73eb68ed
Catherine <cat.taylor.taylor@btinternet.com>
2017-05-20 12:59:47 +0100

    Added in my file.

---
+++ Assets/Scripts/List_of_briefs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task
+{
+    public string TaskName;
+}
+public class List_of_briefs : MonoBehaviour {
+	
+    public List<Task> TaskList;
+    public Transform contentPanel;
+
+	// Use this for initialization
+	void Start () {
+		RefreshDisplay ();
+	}
+	void RefreshDisplay(){
+		RemoveButtons();
+		AddButtons();
+	}
+	private void RemoveButtons(){
+
+	}
+	private void AddButtons(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TaskButtons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TaskButtons : MonoBehaviour {
+
+	public Button buttonComponent;
+    public Text nameLabel;
+  
+    private Task task;
+    private List_of_briefs scrollList;
+    
+
+	// Use this for initialization
+	void Start () {
+		buttonComponent.onClick.AddListener (HandleClick);
+	}
+	 public void HandleClick()
+    {
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
