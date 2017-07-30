7786b7fa1091d72bdac77ac9b4e94c9df6a04044
Owen Morgan <o.morgan@bath.ac.uk>
2017-07-30 16:26:27 +0100

    Added intermediate workchunk class.

---
+++ Assets/Scripts/WorkChunk.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkChunk{
+    float currenttime = 0f;
+    float duration;
+    public Brief b;
+
+    WorkChunk(float _duration, Brief _b){
+        b = _b;
+        duration = _duration;
+    }
+
+    
+}
